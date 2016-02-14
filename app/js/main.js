81dbee7af94143eb8bc37d383c9566641f0beadb
Brendan Dugan <dugan_brendan@yahoo.com>
2016-02-14 11:44:43 -0800

    simplify extending three

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
 define(function (require) {
+  (function _decorateThree () {
+    require('./StereoEffect')();
+    require('./OrbitControls')();
+    require('./DeviceOrientationControls')();
+  })();
   var Text2D = require('./Text2DLib/index');
   var renderLoop = require('./renderLoop');
-  //var ThreeWrapper = require('./StereoEffect');
-  //console.log("THREEWRAPPER: ", ThreeWrapper);
   console.log(Text2D);
   renderLoop.run();
 });
