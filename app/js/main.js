c88941bca5149fc27d0f4db031bc7514cb2fe3ca
Jason Hilton <jasface901@gmail.com>
2016-02-17 20:15:56 -0800

    Moved Steropic effect outside of render loop

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,9 @@
 define(function (require) {
   (function _decorateThree () {
-    require('./StereoEffect')();
     require('./OrbitControls')();
     require('./DeviceOrientationControls')();
   })();
+  THREE.CardBoardEffect = require('./CardBoardEffect');
   var Text2D = require('./Text2DLib/index');
   var renderLoop = require('./renderLoop');
   console.log(Text2D);
